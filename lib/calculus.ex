96ea9d4951fca7855930d5f2b6d52103c8f04675
timcf <timcf@89-235-231-114.saturn.infonet.ee>
2019-08-04 21:43:35 +0300

    huge performance optimization

--- lib/calculus.ex
+++ lib/calculus.ex
@@ -53,10 +53,10 @@ defmodule Calculus do
 
   iex> u = User.new(id: 1, name: "Jessy")
   iex> u.(:get_name, :fake_security_key)
-  ** (RuntimeError) For instance of the type Calculus got unsupported METHOD=:get_name with SECURITY_KEY=:fake_security_key
+  ** (RuntimeError) For value of the type User got unsupported METHOD=:get_name with SECURITY_KEY=:fake_security_key
 
   iex> User.get_name(&({&2, &1}))
-  ** (RuntimeError) Instance of the type User can't be created in other module CalculusTest
+  ** (RuntimeError) Value of the type User can't be created in other module CalculusTest
   ```
   """
 
@@ -83,29 +83,38 @@ defmodule Calculus do
   end
 
   defmacro defcalculus(quoted_state, do: raw_eval_clauses) do
-    pre_defined_eval_clauses =
+    first_defined_eval_clauses =
       quote location: :keep do
+        :return, @security_key ->
+          calculus(state: state, return: return)
+
         :is?, @security_key ->
           calculus(state: state, return: true)
-
-        method, security_key ->
-          raise(
-            "For instance of the type #{inspect(__MODULE__)} got unsupported METHOD=#{
-              inspect(method)
-            } with SECURITY_KEY=#{inspect(security_key)}"
-          )
       end
 
-    eval_clauses =
+    middle_eval_clauses =
       raw_eval_clauses
       |> case do
         {:__block__, _, []} -> []
         [_ | _] -> raw_eval_clauses
       end
       |> Enum.map(&add_security_key/1)
-      |> Enum.concat(pre_defined_eval_clauses)
 
-    eval_fn = {:fn, [], eval_clauses}
+    last_eval_clauses =
+      quote location: :keep do
+        method, security_key ->
+          raise(
+            "For value of the type #{inspect(__MODULE__)} got unsupported METHOD=#{
+              inspect(method)
+            } with SECURITY_KEY=#{inspect(security_key)}"
+          )
+      end
+
+    eval_fn = {
+      :fn,
+      [],
+      first_defined_eval_clauses ++ middle_eval_clauses ++ last_eval_clauses
+    }
 
     quote location: :keep do
       @security_key 64 |> :crypto.strong_rand_bytes() |> Base.encode64() |> String.to_atom()
@@ -113,23 +122,19 @@ defmodule Calculus do
       @opaque t :: __MODULE__.t()
 
       defmacrop calculus(state: state, return: return) do
-        cmod = unquote(__MODULE__)
-
         quote location: :keep do
-          unquote(cmod).new(unquote(state), unquote(return))
+          {unquote(state), unquote(return)}
         end
       end
 
       defmacrop calculus(return: return, state: state) do
-        cmod = unquote(__MODULE__)
-
         quote location: :keep do
-          unquote(cmod).new(unquote(state), unquote(return))
+          {unquote(state), unquote(return)}
         end
       end
 
       defmacrop calculus(some) do
-        "Calculus expect keyword list, example: calculus(state: foo, return: bar). But got term #{
+        "Calculus expression expect keyword list, example: calculus(state: foo, return: bar), but got term #{
           inspect(some)
         }"
         |> raise
@@ -139,20 +144,18 @@ defmodule Calculus do
         case Function.info(it, :module) do
           {:module, __MODULE__} ->
             #
-            # TODO : test that "state" can not be overriden in "quoted_state" expression
+            # TODO : test that "state" and "return" can not be overriden in "quoted_state" expression
             #
-            cs = it.(method, @security_key)
-            state = unquote(__MODULE__).state(cs)
+            calculus(state: state, return: return) = it.(method, @security_key)
             unquote(quoted_state) = state
-            unquote(__MODULE__).new(unquote(eval_fn), unquote(__MODULE__).return(cs))
 
-          {:module, unquote(__MODULE__)} ->
-            it
-            |> unquote(__MODULE__).state()
-            |> eval(method)
+            case method do
+              :return -> return
+              _ -> unquote(eval_fn)
+            end
 
           {:module, module} ->
-            "Instance of the type #{inspect(__MODULE__)} can't be created in other module #{
+            "Value of the type #{inspect(__MODULE__)} can't be created in other module #{
               inspect(module)
             }"
             |> raise
@@ -165,7 +168,7 @@ defmodule Calculus do
       """
       @spec return(__MODULE__.t()) :: term
       def return(it) do
-        unquote(__MODULE__).return(it)
+        eval(it, :return)
       end
 
       @doc """
@@ -184,85 +187,13 @@ defmodule Calculus do
       end
 
       defmacrop construct(state) do
-        cmod = unquote(__MODULE__)
-
         quote location: :keep do
           fn :new, @security_key ->
-            unquote(cmod).new(unquote(state), :ok)
+            calculus(state: unquote(state), return: :ok)
           end
           |> eval(:new)
         end
       end
     end
-  end
-
-  #
-  # define calculus data type as church-encoded type as well
-  #
-
-  require Record
-
-  Record.defrecordp(:calculus,
-    state: nil,
-    return: nil
-  )
-
-  @security_key 64 |> :crypto.strong_rand_bytes() |> Base.encode64() |> String.to_atom()
-
-  defp eval(it0, method) do
-    case Function.info(it0, :module) do
-      {:module, __MODULE__} ->
-        calculus(
-          state: calculus(state: state1, return: return1) = state0,
-          return: return0
-        ) = it0.(method, @security_key)
-
-        it1 = fn
-          :state, @security_key ->
-            calculus(state: state0, return: state1)
-
-          :return, @security_key ->
-            calculus(state: state0, return: return1)
-
-          method, security_key ->
-            raise(
-              "For instance of the type #{inspect(__MODULE__)} got unsupported METHOD=#{
-                inspect(method)
-              } with SECURITY_KEY=#{inspect(security_key)}"
-            )
-        end
-
-        calculus(state: it1, return: return0)
-
-      {:module, module} ->
-        "Instance of the type #{inspect(__MODULE__)} can't be created in other module #{
-          inspect(module)
-        }"
-        |> raise
-    end
-  end
-
-  def new(state0, return0) do
-    calculus(state: state1, return: :ok) =
-      fn
-        :new, @security_key ->
-          calculus(
-            state: calculus(state: state0, return: return0),
-            return: :ok
-          )
-      end
-      |> eval(:new)
-
-    state1
-  end
-
-  def state(it) do
-    calculus(state: ^it, return: return) = eval(it, :state)
-    return
-  end
-
-  def return(it) do
-    calculus(state: ^it, return: return) = eval(it, :return)
-    return
   end
 end
