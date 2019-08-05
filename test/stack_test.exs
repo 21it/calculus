4fdec5b7880d58c6bf421b1b997b7a61baa8882a
tim2CF <tim4CF@gmail.com>
2019-08-05 20:25:43 +0300

    some ExUnit tests for Stack example

---
+++ test/stack_test.exs
@@ -0,0 +1,82 @@
+defmodule StackTest do
+  use ExUnit.Case
+
+  setup do
+    {
+      :ok,
+      %{stack: Stack.new([1, 2, 3])}
+    }
+  end
+
+  test "Stack.is?/1 method", %{stack: x} do
+    assert Stack.is?(x)
+    refute Stack.is?([1, 2, 3])
+  end
+
+  test "Stack.return/1 method", %{stack: x} do
+    assert :ok == Stack.return(x)
+  end
+
+  test "Stack.new/1 smart constructor success" do
+    x = Stack.new([1, 2, 3])
+    assert Stack.is?(x)
+    assert is_function(x, 2)
+  end
+
+  test "Stack.new/1 smart constructor fail" do
+    assert_raise FunctionClauseError,
+                 "no function clause matching in Stack.new/1",
+                 fn ->
+                   Stack.new(:BANG)
+                 end
+  end
+
+  test "Stack.push/2 method", %{stack: x0} do
+    x1 = Stack.push(x0, 99)
+    assert Stack.is?(x1)
+    assert is_function(x1, 2)
+    assert :ok == Stack.return(x1)
+    assert x1 != x0
+  end
+
+  test "Stack.pop/1 method", %{stack: x0} do
+    x1 = Stack.pop(x0)
+    assert Stack.is?(x1)
+    assert is_function(x1, 2)
+    assert {:ok, 1} == Stack.return(x1)
+    assert x1 != x0
+  end
+
+  test "Can chain methods", %{stack: x0} do
+    x1 =
+      x0
+      |> Stack.pop()
+      |> Stack.pop()
+      |> Stack.pop()
+
+    assert Stack.is?(x1)
+    assert is_function(x1, 2)
+    assert {:ok, 3} == Stack.return(x1)
+    assert x1 != x0
+  end
+
+  test "Can chain methods to return", %{stack: x} do
+    assert {:ok, 99} ==
+             x
+             |> Stack.push(99)
+             |> Stack.pop()
+             |> Stack.return()
+  end
+
+  test "Can not pop empty stack", %{stack: x0} do
+    x1 =
+      x0
+      |> Stack.pop()
+      |> Stack.pop()
+      |> Stack.pop()
+      |> Stack.pop()
+
+    assert {:error, :empty_stack} == Stack.return(x1)
+    assert x1 == Stack.pop(x1)
+  end
+end
